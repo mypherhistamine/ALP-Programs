8da48059d5163615b4445f1eabd1ef9d3718982c
Rishabh Mishra <rishabhmishra23599@gmail.com>
2021-08-24 20:11:31 +0530

    added questions

---
+++ examples/assembler_directives.asm
@@ -0,0 +1,11 @@
+;assembler directives 
+;assembler will execute the program wrt to the assembler directive
+;They are instructions entered into the source code along with the 
+; assembly language.
+;They are Psuedo instructions (They do not get translated into object code)
+; but are used as special instructions to the assembler to perform some special functions
+;they control the generation of machine code and organization of the program
+;There are two types of instructions                                        
+
+
+
